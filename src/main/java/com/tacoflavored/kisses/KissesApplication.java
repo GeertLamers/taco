13342da2bd1232368231d3b67ccecb348d5a41cd
Geert Lamers <wickeh@gmail.com>
2018-05-02 00:00:49 +0200

    chapter 1 finished up

---
+++ src/main/java/com/tacoflavored/kisses/KissesApplication.java
@@ -0,0 +1,12 @@
+package com.tacoflavored.kisses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KissesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KissesApplication.class, args);
+    }
+}
